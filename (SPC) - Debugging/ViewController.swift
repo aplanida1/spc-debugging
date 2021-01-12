1152db21022f38fe237d4d5c9ed34c72338885ef
Albert <albert@buildmycrm.com>
2021-01-11 19:16:54 -0800

    Initial Commit

---
+++ (SPC) - Debugging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  (SPC) - Debugging
+//
+//  Created by Albert Planida on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
